c2ded0c6bf9851597445fd43f6f8e2170082f7ec
krutarseniy30 <71847651+krutarseniy30@users.noreply.github.com>
2025-04-25 23:34:28 +0300

    Update app.py

--- app.py
+++ app.py
@@ -12,14 +12,10 @@ def distance():
     data = request.get_json()
     start_point = tuple(data['start'])  # Получаем начальную точку
     end_point = tuple(data['end'])      # Получаем конечную точку
-    start_airport = data['startAirport']  # Получаем название начального аэропорта
-    end_airport = data['endAirport']      # Получаем название конечного аэропорта
     
     dist = great_circle(start_point, end_point).kilometers
     result = {
         'distance': round(dist, 2),
-        'startAirport': start_airport,
-        'endAirport': end_airport
     }
     return jsonify(result), 200
 
